895fb7cfd37ce5fd3d4afc1ca819a815ef41c587
unknown <kidmortal@gmail.com>
2021-04-29 15:00:40 -0300

    eu n aguento

--- src/pages/api/mongodb/usuarios/index.ts
+++ src/pages/api/mongodb/usuarios/index.ts
@@ -1,15 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { connectToCachedDb, connectToNewDb } from "../../../../util/mongodb";
 
-export default async (req: NextApiRequest, res: NextApiResponse, callback) => {
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  context,
+  callback
+) => {
   const { email } = req.query;
   let user = await GetOneUser(email);
+  console.log("conseguiu user");
+  console.log(user);
   res.status(200).json(user);
+  console.log("respondeu res status 200");
+  context.callbackWaitsForEmptyEventLoop = false;
+  console.log("context");
   callback(null, {
     isBase64Encoded: false,
     statusCode: 200,
     body: JSON.stringify(user),
   });
+  console.log("callback");
 };
 
 async function GetOneUser(email) {
@@ -19,12 +30,10 @@ async function GetOneUser(email) {
     Client = await connectToCachedDb();
     user = await Client.db().collection("usuarios").findOne({ email });
     console.log("Using Cached Connection");
-    console.log(user);
   } catch (error) {
     Client = await connectToNewDb();
     user = await Client.db().collection("usuarios").findOne({ email });
     console.log("Created new connection");
-    console.log(user);
   }
   return user;
 }
