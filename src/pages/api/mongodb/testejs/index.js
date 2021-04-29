c41f522b9c0a7f004b2f30acfe8f01117e4a9f4e
unknown <kidmortal@gmail.com>
2021-04-29 14:30:12 -0300

    vai

--- src/pages/api/mongodb/testejs/index.js
+++ src/pages/api/mongodb/testejs/index.js
@@ -1,6 +1,8 @@
-exports.handler = async function (event, context) {
+export default async (req, res) => {
+  console.log("Request Ts");
+  res.status(200).json("oi");
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: "oi",
   };
 };
--- src/pages/api/mongodb/testets/index.ts
+++ src/pages/api/mongodb/testets/index.ts
@@ -1,6 +1,10 @@
-export async function handler(event, context) {
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("Request Ts");
+  res.status(200).json("oi");
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: "oi",
   };
-}
+};
