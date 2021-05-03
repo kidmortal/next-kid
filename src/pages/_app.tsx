d6903e47df026709fee3430a362fe15179af8cb5
Kidmortal <kidmortal@gmail.com>
2021-05-03 20:58:00 -0300

    remove server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { AppContextProvider } from "../context/AppContext";
 import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
+
   return (
     <AppContextProvider>
       <ChakraProvider theme={theme}>
