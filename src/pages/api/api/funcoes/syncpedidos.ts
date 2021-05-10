14b11db9ac3460eac9a143e867a0574258ecb246
unknown <kidmortal@gmail.com>
2021-05-10 12:06:56 -0300

    sei la, fix

--- src/pages/api/api/funcoes/syncpedidos.ts
+++ src/pages/api/api/funcoes/syncpedidos.ts
@@ -4,12 +4,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { email } = req.body;
   if (!email) return res.status(200).json({ erro: "Usuario não possui email" });
-  await axios.get(
-    `${process.env.API_URL}/syncContas?key=${process.env.API_KEY}&email=${email}`
-  );
   res
     .status(200)
     .json(
       "Solicitado com sucesso, você ira receber em breve uma notificação no WhatsApp"
     );
+  await axios.get(
+    `${process.env.API_URL}/syncContas?key=${process.env.API_KEY}&email=${email}`
+  );
 };
